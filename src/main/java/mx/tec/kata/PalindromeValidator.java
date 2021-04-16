7189e8b881e41d7d6f6178f901dbafa7dae36477
Alejandro Perez <alex98.perez@hotmail.com>
2021-04-16 17:06:37 -0600

    step two

--- src/main/java/mx/tec/kata/PalindromeValidator.java
+++ src/main/java/mx/tec/kata/PalindromeValidator.java
@@ -7,8 +7,14 @@ public class PalindromeValidator {
 			return true;
 			
 		}
-		
-		return false;		
+		else if(string.length() == 2 && string.charAt(0) == string.charAt(1)){
+			
+			return true;			
+		}
+		else {
+			return false;
+		}
+			
 	}
 
 }

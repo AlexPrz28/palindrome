444891deb463d4f40d1fe3eb0cba93ec8171deb1
Alejandro Perez <alex98.perez@hotmail.com>
2021-04-16 17:22:18 -0600

    step four

--- src/test/java/mx/tec/kata/PalindromeValidatorTest.java
+++ src/test/java/mx/tec/kata/PalindromeValidatorTest.java
@@ -61,5 +61,31 @@ class PalindromeValidatorTest {
         assertEquals(expectedResult, actualResult);
 
     }
+	
+	@Test
+    void Given_4LetterPalindromeString_When_ValidatingPalindrome_Then_True() {
+        // Given
+        String string = "abba";
+        // When
+        boolean actualResult = validator.validate(string);
+
+        // Then
+        boolean expectedResult = true;
+        assertEquals(expectedResult, actualResult);
+
+    }
+	
+	@Test
+    void Given_4LetterNotPalindromeString_When_ValidatingPalindrome_Then_False() {
+        // Given
+        String string = "abda";
+        // When
+        boolean actualResult = validator.validate(string);
+
+        // Then
+        boolean expectedResult = false;
+        assertEquals(expectedResult, actualResult);
+
+    }
 
 }
